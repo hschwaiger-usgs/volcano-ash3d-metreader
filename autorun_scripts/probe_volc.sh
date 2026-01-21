0e792842ce2e8650e7ea7309f44847d7de94edfe
Hans <hschwaiger@usgs.gov>
2026-01-21 14:43:12 -0900

    Corrected path error in script.

--- autorun_scripts/probe_volc.sh
+++ autorun_scripts/probe_volc.sh
@@ -173,7 +173,7 @@ do
   
     ln -s ${WINDPATH}/${WINDFILE} .
     echo "${USGSROOT}/bin/MetProbe ${WINDFILE} ${ARGS}"
-    ${USGSROOT}/bin/MetProb ${WINDFILE} ${ARGS}
+    ${USGSROOT}/bin/MetProbe ${WINDFILE} ${ARGS}
     HourOffset=`echo "${FChour} + ${t}"  | bc`
     NewYYYYMMDD=`date -d"${yearmonthday} +${HourOffset} hour" -u +%Y%m%d`
     Newhour=`date -d"${yearmonthday} +${HourOffset} hour" -u +%H`
